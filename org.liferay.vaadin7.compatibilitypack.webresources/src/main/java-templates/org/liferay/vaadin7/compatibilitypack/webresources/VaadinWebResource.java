f4410d8e9bead1f666493204c67606634fc83014
Sampsa Sohlman <sampsa@sohlman.com>
2016-02-07 01:50:22 +0200

    Modular web resources

---
+++ org.liferay.vaadin7.compatibilitypack.webresources/src/main/java-templates/org/liferay/vaadin7/compatibilitypack/webresources/VaadinWebResource.java
@@ -0,0 +1,5 @@
+package org.liferay.vaadin7.compatibilitypack.webresources;
+
+public abstract class VaadinWebResource {
+	public static final String JAVAX_PORTLET_RESOURCES_PATH = "javax.portlet.init-param.vaadin.resources.path=/o/vaadin${vaadin.version}";
+}
