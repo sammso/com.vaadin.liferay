3d862697880583758c351fdf9b04aafc188a0232
Sampsa Sohlman <sampsa@sohlman.com>
2016-03-21 14:20:13 +0200

    Remove need of VaadinPortlet at OSGi projects

---
+++ org.liferay.vaadin7.compatibilitypack.webresources/src/main/java/org/liferay/vaadin7/compatibilitypack/OSGiUIProvider.java
@@ -0,0 +1,66 @@
+package org.liferay.vaadin7.compatibilitypack;
+
+import org.osgi.framework.ServiceObjects;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.vaadin.server.ClientConnector.DetachEvent;
+import com.vaadin.server.ClientConnector.DetachListener;
+import com.vaadin.server.UIClassSelectionEvent;
+import com.vaadin.server.UICreateEvent;
+import com.vaadin.server.UIProvider;
+import com.vaadin.ui.UI;
+
+public class OSGiUIProvider extends UIProvider {
+	public OSGiUIProvider(ServiceObjects<UI> serviceObjects) {
+		super();
+		_serviceObjects = serviceObjects;
+		_ui = serviceObjects.getService();
+		_uiClass = (Class<UI>)_ui.getClass();
+	}
+
+	@Override
+	public Class<? extends UI> getUIClass(UIClassSelectionEvent event) {
+		return _uiClass;
+	}
+	
+	@Override
+	public UI createInstance(UICreateEvent event) {
+		final ServiceObjects<UI> serviceObjects = _serviceObjects;
+		final UI ui = serviceObjects.getService();
+		
+		ui.addDetachListener(new DetachListener() {
+			@Override
+			public void detach(DetachEvent event) {
+				serviceObjects.ungetService(ui);
+				if (_log.isDebugEnabled()) {
+					_log.debug("unregistered UI " + ui.toString());
+				}	
+			}
+		} );
+		return ui;
+	}
+	
+	public UI getDefaultUI() {
+		return _ui;
+	}
+	
+	public String getDefaultDisplayName() {
+		String name = _uiClass.getName();
+		int beginIndex = name.lastIndexOf(".");
+		beginIndex = beginIndex > 0 ? beginIndex + 1 : 0;
+		
+		name = name.substring(beginIndex);
+		
+		return name;
+	}
+	
+	public String getPortletName() {
+		return _uiClass.getName();		
+	}
+
+	private Log _log = LogFactoryUtil.getLog(OSGiUIProvider.class);
+	private ServiceObjects<UI> _serviceObjects;
+	private Class<UI> _uiClass;
+	private UI _ui;
+}
---
+++ org.liferay.vaadin7.compatibilitypack.webresources/src/main/java/org/liferay/vaadin7/compatibilitypack/VaadinOSGiPortlet.java
@@ -0,0 +1,51 @@
+package org.liferay.vaadin7.compatibilitypack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.portlet.PortletException;
+
+import com.vaadin.server.DefaultUIProvider;
+import com.vaadin.server.ServiceException;
+import com.vaadin.server.SessionInitEvent;
+import com.vaadin.server.SessionInitListener;
+import com.vaadin.server.UIProvider;
+import com.vaadin.server.VaadinPortlet;
+import com.vaadin.server.VaadinSession;
+
+public class VaadinOSGiPortlet extends VaadinPortlet {
+	public VaadinOSGiPortlet(UIProvider uiProvider) {
+		_uUiProvider = uiProvider;
+	}
+	
+	@Override
+	protected void portletInitialized() throws PortletException {
+        getService().addSessionInitListener(new SessionInitListener() {
+
+			private static final long serialVersionUID = 1737347072701619179L;
+
+			@Override
+            public void sessionInit(SessionInitEvent sessionInitEvent)
+                    throws ServiceException {
+				
+				// Implementation is copied from vaadin-spring
+				
+                VaadinSession session = sessionInitEvent.getSession();
+                List<UIProvider> uiProviders = new ArrayList<UIProvider>(
+                        session.getUIProviders());
+                for (UIProvider provider : uiProviders) {
+                    // use canonical names as these may have been loaded with
+                    // different classloaders
+                    if (DefaultUIProvider.class.getCanonicalName().equals(
+                            provider.getClass().getCanonicalName())) {
+                        session.removeUIProvider(provider);
+                    }
+                }
+                
+                session.addUIProvider(_uUiProvider);
+            }
+        });
+	}
+	
+	private UIProvider _uUiProvider;
+}
