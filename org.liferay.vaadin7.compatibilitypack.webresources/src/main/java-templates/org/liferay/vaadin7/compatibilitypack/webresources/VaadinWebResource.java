7e2e48c818d6ee38e44ac2cb83c2abdcf573437d
Sampsa Sohlman <sampsa@sohlman.com>
2016-03-21 14:19:04 +0200

    Refactor VaadinWebResource

--- org.liferay.vaadin7.compatibilitypack.webresources/src/main/java-templates/org/liferay/vaadin7/compatibilitypack/webresources/VaadinWebResource.java
+++ org.liferay.vaadin7.compatibilitypack.webresources/src/main/java-templates/org/liferay/vaadin7/compatibilitypack/webresources/VaadinWebResource.java
@@ -1,5 +1,15 @@
 package org.liferay.vaadin7.compatibilitypack.webresources;
 
+/**
+ * This class contains constant to find current resources
+ * @author Sampsa Sohlman
+ */
 public abstract class VaadinWebResource {
-	public static final String JAVAX_PORTLET_RESOURCES_PATH = "javax.portlet.init-param.vaadin.resources.path=/o/vaadin${vaadin.version}";
+	public static final String JAVAX_PORTLET_RESOURCES_INIT_PARAM = 
+		"javax.portlet.init-param.vaadin.resources.path";
+	public static final String JAVAX_PORTLET_RESOURCES_INIT_VALUE = 
+		"/o/vaadin${vaadin.version}";
+	public static final String JAVAX_PORTLET_RESOURCES_PATH = 
+		JAVAX_PORTLET_RESOURCES_INIT_PARAM + "=" + 
+		JAVAX_PORTLET_RESOURCES_INIT_VALUE;
 }
